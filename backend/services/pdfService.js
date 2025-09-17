1247a2463662dbe0d66382e66450fcd4c4ec8410
JuanPerezC893 <229856071+JuanPerezC893@users.noreply.github.com>
2025-09-16 23:18:45 -0300

    add gen pdf fix vercel

--- backend/services/pdfService.js
+++ backend/services/pdfService.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 const path = require("path");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 async function crearPDF(proyecto, residuos) {
   try {
@@ -65,7 +66,12 @@ async function crearPDF(proyecto, residuos) {
     }
 
     // 5. Generar PDF con Puppeteer
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
 
